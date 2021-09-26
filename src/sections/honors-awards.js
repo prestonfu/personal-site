33c312bdc9d00caa5aacf85a36f135a05d0af11d
prestonfu <prestonmfu@gmail.com>
2021-09-25 18:00:34 -0700

    Minor updates

--- src/sections/honors-awards.js
+++ src/sections/honors-awards.js
@@ -15,10 +15,10 @@ const HonorsAwards = () => {
           <div className="col-start-2 mt-12 px-8 md:mt-0 col-end-2 md:pl-6 md:pr-16">
             <div className="flex justify-center md:justify-start flex-wrap">
               <div className="tag-outline-wrapper">
-                <p className="text-xs sm:text-sm font-inter">USA Junior Math Olympiad Top 80 Nationally 2020</p>
+                <p className="text-xs sm:text-sm font-inter">USA Junior Math Olympiad Top 80 Nationally, 2020</p>
               </div>
               <div className="tag-outline-wrapper">
-                <p className="text-xs sm:text-sm font-inter">USA Physics Olympiad Honorable Mention, Top 150 Nationally, 2021</p>
+                <p className="text-xs sm:text-sm font-inter">USA Physics Olympiad Honorable Mention Top 150 Nationally, 2021</p>
               </div>
               <div className="tag-outline-wrapper">
                 <p className="text-xs sm:text-sm font-inter">USA Computing Olympiad Gold Division, 2018</p>
