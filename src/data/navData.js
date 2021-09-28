2f01fadddcbaa1b304b875db83d209e1f1c2fbce
prestonfu <prestonmfu@gmail.com>
2021-09-28 16:15:14 -0700

    Failed attempt at fixing navbar
    Will come back to it later; temporarily under construction

---
+++ src/data/navData.js
@@ -0,0 +1,29 @@
+const NavData = [
+  {
+    index: true,
+    label: "PRESTON FU",
+    path: '/',
+  },
+  {
+    label: 'Math',
+    path: '/projects/math',
+  },
+  {
+    label: 'Computer Science',
+    path: '/projects/cs',
+  },
+  {
+    label: 'Writing',
+    path: '/projects/writing',
+  },
+  {
+    label: 'Resume',
+    path: '/files/Resume.pdf',
+  },
+  {
+    label: 'Contact',
+    path: '#contact',
+  },
+];
+
+export default NavData;
