3ae02ba2cfa9efcfc9425ac8a119ca516dcd5427
prestonfu <prestonmfu@gmail.com>
2021-10-05 23:03:00 -0700

    Leadership update

---
+++ src/pages/projects/Leadership.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import ProjectCard from '../../components/Projects/project-card';
+import projectData from '../../data/projects/projectDataLeadership.json'
+
+const projData = projectData
+
+const Projects = () => {
+  return (
+    <div className="h-auto">
+      <div className="h-20" />
+      <div className="mx-auto max-w-6xl sm:pl-16 z-0">
+        <div className="h-12" />
+        <h2 className = "text-center sm:text-center page-header">
+          <b>LEADERSHIP</b>
+        </h2>
+        <div className="px-8 sm:pr-16 sm:pl-0 mt-12">
+            {projData.map((data) => (
+              <div id={data.projectID}>
+                <ProjectCard
+                  projectName = {data.projectName}
+                  techStack = {data.techStack}
+                  description = {data.description}
+                  imgSrc = {data.imgSrc}
+                  projectLink = {data.projectLink}
+                />
+                <div className="h-12"></div>
+              </div>
+            ))}
+        </div>
+        <div className="h-12" />
+      </div>
+    </div>
+  )
+}
+
+export default Projects
