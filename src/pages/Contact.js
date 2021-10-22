3952cf55d44fc15883af39df07fc16d1386b73be
prestonfu <prestonmfu@gmail.com>
2021-10-22 11:37:53 -0700

    Create Contact.js

---
+++ src/pages/Contact.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import data from '../data/contactIconData';
+
+
+const NotFound = () => {
+  return (
+    <div>
+      <div className="relative h-screen flex items-center">
+
+        <div className="mx-auto max-w-6xl sm:pl-16 z-0 justify-center">
+          {/* <div className="hero-grid"> */}
+          <h1 className="hero-header">
+            <b>Contact</b>
+          </h1>
+          <div className="h-8" />
+          <p>
+            Feel free to get in touch! Contact me at <a className="link" href="mailto:prestonmfu@gmail.com">
+              prestonmfu@gmail.com
+            </a>.
+          </p>
+          <div className="h-4" />
+
+          <ul className="icons">
+            {data.map((s) => (
+              <a className="py-6 mr-5 page-contact-icon-link" href={s.link} target="_blank">
+                <FontAwesomeIcon icon={s.icon} />
+              </a>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default NotFound
