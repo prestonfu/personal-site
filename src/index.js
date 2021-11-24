2c3692c490b42b20d50e7b9e90d9aa22585a6945
prestonfu <prestonmfu@gmail.com>
2021-11-24 02:35:39 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './static/css/main.scss';
 import App from './App.js';
-import ReactGA from "react-ga";
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('G-R5LM1906J7');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(
   <React.StrictMode>
